b1586ec416d634823cf11eda4c0772604f97153a
Vladimir Minakov <Vladimirmvk@gmail.com>
2023-03-04 17:47:27 +0200

    no Prompt

--- src/index.js
+++ src/index.js
@@ -13,36 +13,36 @@ import {callGptApi} from './js/gpt-service.js';
 
 
 
-import { PromptEngine } from "prompt-engine";
-const description = "I want to speak with a bot which replies in under 20 words each time";
-const examples = [
-  { input: "Hi", response: "I'm a chatbot. I can chat with you about anything you'd like." },
-  { input: "Can you help me with the size of the universe?", response: "Sure. The universe is estimated to be around 93 billion light years in diameter." },
-];
-const flowResetText = "Forget the earlier conversation and start afresh";
+// import { PromptEngine } from "prompt-engine";
+// const description = "I want to speak with a bot which replies in under 20 words each time";
+// const examples = [
+//   { input: "Hi", response: "I'm a chatbot. I can chat with you about anything you'd like." },
+//   { input: "Can you help me with the size of the universe?", response: "Sure. The universe is estimated to be around 93 billion light years in diameter." },
+// ];
+// const flowResetText = "Forget the earlier conversation and start afresh";
 
-const promptEngine = new PromptEngine(description, examples, flowResetText, {
-  modelConfig: {
-    maxTokens: 512,
-  }
-});
+// const promptEngine = new PromptEngine(description, examples, flowResetText, {
+//   modelConfig: {
+//     maxTokens: 512,
+//   }
+// });
 
-promptEngine.addInteractions([
-  { 
-    input: "What is the size of an SUV in general?",
-    response: "An SUV typically ranges from 16 to 20 feet long."
-  },
-]);
+// promptEngine.addInteractions([
+//   { 
+//     input: "What is the size of an SUV in general?",
+//     response: "An SUV typically ranges from 16 to 20 feet long."
+//   },
+// ]);
 
-promptEngine.removeLastInteraction()
+// promptEngine.removeLastInteraction()
 
-promptEngine.addInteraction("What is the maximum speed an SUV from a performance brand can achieve?", 
-"Some performance SUVs can reach speeds over 150mph.");
+// promptEngine.addInteraction("What is the maximum speed an SUV from a performance brand can achieve?", 
+// "Some performance SUVs can reach speeds over 150mph.");
 
-const outputPrompt = promptEngine.buildPrompt("Can some cars reach higher speeds than that?");
+// const outputPrompt = promptEngine.buildPrompt("Can some cars reach higher speeds than that?");
 
-console.log("PROMPT Ответ\n\n" + outputPrompt);
-console.log("PROMPT LENGTH: " + outputPrompt.length);
+// console.log("PROMPT Ответ\n\n" + outputPrompt);
+// console.log("PROMPT LENGTH: " + outputPrompt.length);
 
 
 
